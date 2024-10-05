b89bc4303dcc3d5a901c727199a01f030610b44d
Senai <Senai@CEPEAS21104197>
2024-10-04 21:55:12 -0300

    Add project files.

---
+++ SistemaMatriz/MatrizDeInteiros.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaMatriz
+{
+    internal class MatrizDeInteiros
+    {
+        int[,] matriz;
+
+        public MatrizDeInteiros(int linha, int coluna)
+        {
+            matriz = new int[linha, coluna];
+            for (int i = 0; i < linha; i++)
+            {
+                for (int j = 0; j < coluna; j++)
+                    matriz[i, j] = i;
+            }
+        }
+
+        public string Inserir(int linha, int coluna, int valor)
+        {
+            if (linha > matriz.GetLength(0) || coluna > matriz.GetLength(1))
+            {
+                return "A linha é maior que a coluna";
+            }
+            else
+            {
+                matriz[linha, coluna] = valor;
+                return " O valor foi inserido na matriz";
+            }
+        }
+
+        public string Equadrada()
+        {
+            bool quadrada;
+
+            if (matriz.GetLength(0) == matriz.GetLength(1))
+            {
+                quadrada = true;
+
+            }
+            else
+            {
+                quadrada = false;
+            }
+            return quadrada.ToString();
+        }
+
+        public int Total()
+        {
+            int total = 0;
+
+            for (int i = 0; i < matriz.GetLength(0); i++)
+            {
+                for (int j = 0; j < matriz.GetLength(1); j++)
+                {
+                    total += matriz[i, j];
+                }
+            }
+            return total;
+        }
+
+    }
+}
---
+++ SistemaMatriz/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaMatriz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MatrizDeInteiros mat = new MatrizDeInteiros(3,2);
+            Console.WriteLine(mat.Inserir(10,1,5));
+           
+            Console.WriteLine(mat.Equadrada());
+            Console.WriteLine(mat.Total());
+
+            Console.ReadKey();
+        }
+    }
+}
